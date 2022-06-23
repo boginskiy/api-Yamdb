4413e76d2ae82af583e5e1505b5a96447c12db92
Maria <omn2000@yandex.ru>
2022-06-23 21:32:24 +0300

    Title + api

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .permissions import AdminOrReadOnly
+from .serializers import (CategorySerializer, GenreSerializer,
+                          ReadTitleSerializer, WriteTitleSerializer
+                          )
 
-# Create your views here.
+from titles.models import Category, Genre, Title
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (AdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (AdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = WriteTitleSerializer
+    permission_classes = (AdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('name',)
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return WriteTitleSerializer
+        return ReadTitleSerializer
